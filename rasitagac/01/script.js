a4b8345d86c5437b7ab98708153df574367e59a6
rasitagac <rasitagac@hotmail.com>
2021-05-04 23:04:14 +0300

    Numbers exercise

---
+++ rasitagac/01/script.js
@@ -0,0 +1,30 @@
+//Created by sufuk
+
+alert("Doctor Who ?") // The annoying message thing appears in the top when page loading
+
+// you can acsess console in chrome too with f12
+
+//.log stands for a "log" like message
+console.log("What is better" +
+    "to be born");
+console.log("good or overcome your" +
+    "evil nature" + "through great effort");
+
+console.warn("This is a warning message ");
+
+console.error("This is a error message ");
+
+
+//some basic variable usages
+var a = 12;
+var b = 23;
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("a + b = " + (a + b));
+
+var myArray = [a, b, 1, 3];
+console.log("myArray = " + myArray);
+
+// Example of get elementbyid
+var tempElement = document.getElementById("header_01")
+console.log(tempElement);
---
+++ rasitagac/05 - Exercise/rasit/script.js
@@ -0,0 +1,20 @@
+var num = 15.4546456465465;
+
+console.log(num.toPrecision(3));
+
+console.log(num.toFixed(3));
+
+console.log(Math.floor(num));
+
+console.log(Math.round(num));
+
+console.log(Math.floor(num));
+
+console.log(Math.ceil(num));
+
+console.log(Math.min(1 ,2, 10 , 56 ,20));
+
+console.log(Math.max(1 ,2, 10 , 56 ,20));
+
+window.prompt("")
+
